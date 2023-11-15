87c45576eccdaf3527193aeadfa17ddbe5e2aa2a
NotZarifGithub <flacko.programming@gmail.com>
2023-11-16 00:54:50 +0800

    navbar done

---
+++ client/src/components/common/Navbar.jsx
@@ -0,0 +1,65 @@
+import { TbBinaryTree } from "react-icons/tb";
+import { CiSearch } from "react-icons/ci";
+import { IoReorderTwoOutline } from "react-icons/io5";
+import { CiLogin } from "react-icons/ci";
+
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion"
+
+const Navbar = () => {
+
+  return (
+    <header className="">
+      <section className="max-w-[1200px] mx-auto flex justify-between items-center p-[20px]">
+
+        {/* logo */}
+        <Link className="flex items-center gap-3" to={'/'}>
+          <TbBinaryTree className="text-2xl"/>
+          <h1 className="hidden text-lg font-medium md:inline-block -tracking-[1px]">
+            DevStash
+          </h1>
+        </Link>
+
+        {/* search button */}
+        <form action="" className="flex items-center p-[5px] rounded-xl border shadow-sm ">
+          <input 
+            type="text" 
+            placeholder="search"
+            className="bg-transparent focus:outline-none px-[10px] w-[100px] md:w-[200px] lg:w-[300px]"
+          />
+          <CiSearch />
+        </form>
+
+        {/* navlinks */}
+        <div className="md:w-[120px] flex items-center">
+          
+          {/* links */}
+          <div className="items-center hidden gap-2 md:flex">
+            <Link 
+              className="flex items-center gap-2" 
+              to={'/sign-in'}
+            >
+              <motion.div
+                whileHover={{scale: 1.2 }}
+              >
+                <CiLogin className="text-xl"/>
+              </motion.div>
+              <h1 
+                className="text-lg font-medium -tracking-[1px]"
+              >
+                Sign in
+              </h1> 
+            </Link> 
+          </div>
+          
+          {/* hamburger link */}
+          <button className="md:hidden" >
+            <IoReorderTwoOutline className="justify-end w-full text-3xl"/>
+          </button>
+        </div>
+      </section>
+    </header>
+  )
+}
+
+export default Navbar
