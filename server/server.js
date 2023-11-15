ca85885c6012881d56cd4a2450f3aaaccfe21ec2
NotZarifGithub <flacko.programming@gmail.com>
2023-11-16 03:40:02 +0800

    connected to database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = process.env.PORT
+const uri = process.env.MONGO_URI
 
+// connect to the server
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 })
 
+// connect to database(mongodb)
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Connected to database")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
