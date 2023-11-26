0a6f0f93cac3f57f6ee1584ab980b00bb7c8d269
NotZarif <flacko.programming@gmail.com>
2023-11-26 20:04:37 +0800

    ready to deploy

--- server/server.js
+++ server/server.js
@@ -1,66 +1,63 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const testRouter = require('./routes/userRoute.js')
-const authRouter = require('./routes/authRoute.js')
-const listRouter = require('./routes/listRoute.js')
-const cookieParser = require('cookie-parser')
-const cors = require('cors');
+const testRouter = require('./routes/userRoute.js');
+const authRouter = require('./routes/authRoute.js');
+const listRouter = require('./routes/listRoute.js');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const app = express();
 
 // allow json
-app.use(express.json())
+app.use(express.json());
 
-const PORT = process.env.PORT
-const uri = process.env.MONGO_URI
+const PORT = process.env.PORT;
+const uri = process.env.MONGO_URI;
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Example CORS configuration in your server
 
-app.use(cors({
-  origin: ['https://devstash-sigma.vercel.app', 'https://devstash.onrender.com'],
-  credentials: true, // enable credentials (cookies, authorization headers)
-}));
-
 // user api route
-app.use('/api/user', testRouter)
+app.use('/api/user', testRouter);
 
 // auth api route
-app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter);
 
 // list api route
-app.use('/api/list', listRouter)
+app.use('/api/list', listRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 // create a middleware for catching errors
 app.use((err, req, res, next) => {
-
   // getting the error status code
   const statusCode = err.statusCode || 500;
-
   // getting the error message
-  const message = err.message || "Internal server error"
+  const message = err.message || 'Internal server error';
 
   return res.status(statusCode).json({
     success: false,
     statusCode,
-    message
-  })
-}) 
+    message,
+  });
+});
 
 // connect to database(mongodb)
 mongoose
   .connect(uri)
   .then(() => {
-    
-  // connect to the server
-  app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-  })
-  console.log("Connected to database")
+    // connect to the server
+    app.listen(PORT, () => {
+      console.log(`listening on port ${PORT}`);
+    });
+    console.log('Connected to database');
   })
   .catch((err) => {
-    console.log("Error connecting to the database:",err.message)
-  })
-
+    console.log('Error connecting to the database:', err.message);
+  });
